6b0df8c65cb6ce6f1402ac961aed10f505a24c94
ricko1993 <rickoyogga@gmail.com>
2021-07-24 21:23:07 +0700

    perubahan lagi

--- app.py
+++ app.py
@@ -1,38 +1,21 @@
 from flask import Flask, render_template
-import requests, schedule
+import requests, json
 
 app = Flask (__name__)
 
 def jabar():
-    api_url = "https://api.kawalcorona.com/indonesia/provinsi"
+    api_url = 'https://api.kawalcorona.com/indonesia/provinsi'
     rstl= requests.get(api_url).json()
     cek=rstl[1]
-    ambil_dic= cek ["attributes"]
+    ambil_dic= cek ['attributes']
     a=[ambil_dic]
     return a
 
-realtime_jabar = schedule.every(2).seconds.do(jabar)
-
-#cek schedule
-#while True:
- #   schedule.run_pending()
-
 data_jabar = jabar()
-#print(data_jabar)
 
-
-'''attributes: {FID: 12, Kode_Provi: 32, Provinsi: "Jawa Barat", Kasus_Posi: 277553, Kasus_Semb: 243650,…}
-FID: 12
-Kasus_Meni: 3678
-Kasus_Posi: 277553
-Kasus_Semb: 243650
-Kode_Provi: 32
-Provinsi: "Jawa Barat"
-'''
-@app.route("/")
+@app.route('/')
 def index():
-    #return render_template('TESTLAGI.HTML')
     return render_template('index.html', data_jabar=data_jabar)
 
-if __name__=="__main__":
+if __name__=='__main__':
     app.run(debug=True)
